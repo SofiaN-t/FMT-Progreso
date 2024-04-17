af7f78d1cd91c2c9ac9fbfe1cc3d2f4848e65332
Your Name <example@example.com>
2024-04-17 16:02:00 +0200

    Draft for Dashboard building

---
+++ scripts/streamlit_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+App_title = "Amazonian Deforestation Overlap"
+App_subtitle = "Source: Laboratorio SIG y SR - Instituto SINCHI"
+
+def main():
+    st.set_page_config(App_title)
+    st.title(App_title)
+    st.caption(App_subtitle)
+
+    # LOADING DATA
+
+    # DISPLAY FILTERS AND MAP
+
+    # DISPLAY METRICS
+
+
+
+
+if __name__ == "__main__":
+    main()
