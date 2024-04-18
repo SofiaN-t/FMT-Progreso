1a893c9a980d123dba12c1bf20d87dea48453c4f
Your Name <example@example.com>
2024-04-18 17:52:21 +0200

    Trying to visualize the data and filter it.

--- scripts/streamlit_app.py
+++ scripts/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 App_title = "Amazonian Deforestation Overlap"
 App_subtitle = "Source: Laboratorio SIG y SR - Instituto SINCHI"
@@ -9,6 +10,14 @@ def main():
     st.caption(App_subtitle)
 
     # LOADING DATA
+    Colombia = pd.read_csv(r"C:\Users\lueva\OneDrive\Documentos\EAE\Code\Progreso\Colombian_amazonas.csv")
+    
+    Status = "Perdida"
+    Colombia = Colombia[Colombia["deforestac"] == Status]
+
+    st.write(Colombia.shape)
+    st.write(Colombia.head())
+    st.write(Colombia.columns)
 
     # DISPLAY FILTERS AND MAP
 
